2203f639449494c63244ee8ec88923b758a77dc9
coldone <drew.vfrost@gmail.com>
2022-06-03 11:46:11 +0300

    initial code

--- main.go
+++ main.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/libp2p/go-libp2p"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	node, err := libp2p.New()
+	port := 4000
+	if port <= 0 || port > 65535 {
+		port = 35035
+	}
+	service := fmt.Sprintf("0.0.0.0:%v", port)
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
 	if err != nil {
-		panic(err)
+		log.Printf("ResolveTCPAddr: %s", err.Error())
+		os.Exit(1)
+	}
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		log.Printf("ListenTCP: %s", err.Error())
+		os.Exit(1)
 	}
 
-	// print the node's listening addresses
-	fmt.Println("Listen addresses:", node.Addrs())
-
-	// shut the node down
-	if err := node.Close(); err != nil {
-		panic(err)
+	fmt.Printf("\n\tService started on %s\n\n", tcpAddr.String())
+	for {
+		_, err := listener.Accept()
+		if err != nil {
+			continue
+		}
 	}
-	fmt.Println("closed")
 }
